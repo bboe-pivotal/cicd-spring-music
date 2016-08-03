875ec557e7d4e95044e50a7d701a646d84b6fb48
Bjorn Boe <bboe@pivotal.io>
2016-08-03 08:11:48 -0700

    Changed color of banner to blue

---
+++ go-blue.sh
@@ -0,0 +1,5 @@
+cp src/main/webapp/assets/css/blue-app.css src/main/webapp/assets/css/app.css
+
+git add .
+git commit -m "Changed color of banner to blue"
+git push
---
+++ go-green.sh
@@ -0,0 +1,5 @@
+cp src/main/webapp/assets/css/green-app.css src/main/webapp/assets/css/app.css
+
+git add .
+git commit -m "Changed color of banner to green"
+git push
